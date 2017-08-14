8ba9b0b5fd7dee46a0c9f4e208d422c9b3f79a73
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-08-14 17:59:24 +0200

    Simplified

--- nonlinear_mixedfe.py
+++ nonlinear_mixedfe.py
@@ -10,13 +10,9 @@ modeled by the incompressible Navier-Stokes mass and momentum equations.
 
 def nonlinear_mixedfe(automatic_jacobian):
 
-    # Physical parameters
-    mu = 1.
 
     # Set numerical parameters.
-    m = 20
-
-    mesh = fenics.UnitSquareMesh(m, m, 'crossed')
+    mesh = fenics.UnitSquareMesh(10, 10, 'crossed')
     
     gamma = 1.e-7
     
@@ -44,50 +40,33 @@ def nonlinear_mixedfe(automatic_jacobian):
                 
                 
     # Set boundary conditions.
-    lid = 'near(x[1],  1.)'
-
-    fixed_walls = 'near(x[0],  0.) | near(x[0],  1.) | near(x[1],  0.)'
-
-    bottom_left_corner = 'near(x[0], 0.) && near(x[1], 0.)'
-
     bcs = [
-        fenics.DirichletBC(W.sub(0), fenics.Expression(("1.", "0."), degree=velocity_degree + 1), lid, method='topological'),
-        fenics.DirichletBC(W.sub(0), fenics.Expression(("0.", "0."), degree=velocity_degree + 1), fixed_walls, method='topological'),
-        fenics.DirichletBC(W.sub(1), fenics.Expression("0.", degree=pressure_degree + 1), bottom_left_corner, method='pointwise')]    
+        fenics.DirichletBC(W.sub(0), fenics.Expression(("1.", "0."), degree=velocity_degree + 1),
+            'near(x[1],  1.)', method='topological'),
+        fenics.DirichletBC(W.sub(0), fenics.Expression(("0.", "0."), degree=velocity_degree + 1),
+            'near(x[0],  0.) | near(x[0],  1.) | near(x[1],  0.)', method='topological'),
+        fenics.DirichletBC(W.sub(1), fenics.Expression("0.", degree=pressure_degree + 1),
+            'near(x[0], 0.) && near(x[1], 0.)', method='pointwise')]    
 
     
     # Set nonlinear variational form.
     inner, dot, grad, div, sym = fenics.inner, fenics.dot, fenics.grad, fenics.div, fenics.sym
 
-    def a(mu, u, v):
-
-        def D(u):
-        
-            return sym(grad(u))
-        
-        return 2.*mu*inner(D(u), D(v))
-
-
-    def b(u, q):
-        
-        return -div(u)*q
-        
-
-    def c(w, z, v):
-       
-        return dot(dot(grad(z), w), v)
-        
+    D = lambda u : sym(grad(u))
     
+    a = lambda u, v : 2.*inner(D(u), D(v))
+
+    b = lambda u, q : -div(u)*q
+
+    c = lambda w, z, v : dot(dot(grad(z), w), v)
+
     w = fenics.TrialFunction(W)
             
     u, p = fenics.split(w)
 
     v, q = fenics.TestFunctions(W)
 
-    F = (
-        b(u, q) - gamma*p*q
-        + c(u, u, v) + a(mu, u, v) + b(v, p)
-        )*fenics.dx
+    F = (b(u, q) - gamma*p*q + c(u, u, v) + a(u, v) + b(v, p))*fenics.dx
         
         
     # Set Jacobian bilinear variational form.
@@ -107,12 +86,8 @@ def nonlinear_mixedfe(automatic_jacobian):
 
         du, dp = fenics.split(dw)
 
-        JR = (
-            b(du, q) - gamma*dp*q
-            - (b(u_k, q) - gamma*p_k*q)
-            + c(du, u_k, v) + c(u_k, du, v) + a(mu, du, v) + b(v, dp)
-            + c(u_k, u_k, v) + a(mu, u_k, v) + b(v, p_k)
-            )*fenics.dx
+        JR = (b(du, q) - gamma*dp*q + c(du, u_k, v) + c(u_k, du, v) + a(du, v) + b(v, dp) 
+            - (b(u_k, q) - gamma*p_k*q + c(u_k, u_k, v) + a(u_k, v) + b(v, p_k)))*fenics.dx
     
 
     # Solve the problem.
